6df56f4aeb8542e17e3b1d2bfbcaabfc42cb6722
Jake Jackson <jake@gravitypdf.com>
2019-05-13 20:47:27 +1000

    Fix unit test error due to dynamic page number

--- tests/phpunit/unit-tests/Image/TestListener.php
+++ tests/phpunit/unit-tests/Image/TestListener.php
@@ -86,8 +86,8 @@ class TestListener extends GpdfUnitTestCase {
 		];
 
 		$entry = [
-			'id'      => 2,
-			'form_id' => 1,
+			'id'      => 1,
+			'form_id' => 2,
 		];
 
 		$pdf = [
@@ -106,6 +106,7 @@ class TestListener extends GpdfUnitTestCase {
 		$this->assertEmpty( ob_get_clean() );
 
 		/* Verify the cached copy is served inline */
+		$GLOBALS['wp']->query_vars['page'] = '1';
 		$image_absolute_path = $this->image_common->get_image_path_from_pdf( $pdf_wrapper->get_filename(), $form['id'], $pdf['id'], $entry['id'], $pdf['pdf_to_image_page'] );
 		wp_mkdir_p( dirname( $image_absolute_path ) );
 		copy( __DIR__ . '/../../assets/image/sample.jpg', $image_absolute_path );
@@ -134,8 +135,8 @@ class TestListener extends GpdfUnitTestCase {
 		];
 
 		$entry = [
-			'id'      => 2,
-			'form_id' => 1,
+			'id'      => 1,
+			'form_id' => 2,
 		];
 
 		$pdf = [
