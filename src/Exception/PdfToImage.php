1652bdc0a8bd5ded6a9392ec8bed1ef3063fa642
Jake Jackson <jake@gravitypdf.com>
2020-04-15 09:52:12 +1000

    Update copyright notice

--- src/Exception/PdfToImage.php
+++ src/Exception/PdfToImage.php
@@ -6,7 +6,7 @@ use Exception;
 
 /**
  * @package     Gravity PDF to Image
- * @copyright   Copyright (c) 2019, Blue Liquid Designs
+ * @copyright   Copyright (c) 2020, Blue Liquid Designs
  * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
  * @since       1.0
  */
